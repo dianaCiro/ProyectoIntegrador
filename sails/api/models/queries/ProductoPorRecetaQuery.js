f02701678fa63bb0377d0e83718ca2e9b6b4118c
dianaciro <cathe9463@gmail.com>
2016-11-20 12:20:13 -0500

    Función create productoPorReta funcionando

--- sails/api/models/queries/ProductoPorRecetaQuery.js
+++ sails/api/models/queries/ProductoPorRecetaQuery.js
@@ -4,7 +4,7 @@ module.exports = {
 		ProductoPorRecetaQuery.query('select r.nombre as receta, p.nombre as  producto,pp.cantidadProducto as cantidad,p.unidadDeMedida from productoPorReceta as pp, producto as p, receta as r  where pp.recet=r.id and pp.product=p.id;', callback);
 	},
 	create: function (productosReceta,callback){
-		ProductoPorRecetaQuery.create(productosReceta).exec(callback);
+		ProductoPorReceta.create(productosReceta).exec(callback);
 	}
 
 }
